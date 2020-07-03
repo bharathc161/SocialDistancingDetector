8cf01cfd4afef8ffb2b6042fa38f91d4adcce25b
Bharath Choudhary <43821614+bharathc161@users.noreply.github.com>
2020-07-03 17:50:41 +0530

    Update transform1.py

--- src/transform1.py
+++ src/transform1.py
@@ -1,21 +1,36 @@
-# import the necessary packages
+
 from transform import four_point_transform
 import numpy as np
 import argparse
+import os
 import cv2
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image file")
+"""ap = argparse.ArgumentParser()
+#--image B:\example_01.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]"
+ap.add_argument("-i", "--image", help = "B:\example_01.png")
 ap.add_argument("-c", "--coords",
-	help = "comma seperated list of source points")
-args = vars(ap.parse_args())
+	help = "[(63, 242), (291, 110), (361, 252), (78, 386)]"
+args = vars(ap.parse_args())"""
 # load the image and grab the source coordinates (i.e. the list of
 # of (x, y) points)
 # NOTE: using the 'eval' function is bad form, but for this example
 # let's just roll with it -- in future posts I'll show you how to
 # automatically determine the coordinates without pre-supplying them
-image = cv2.imread(args["image"])
-pts = np.array(eval(args["coords"]), dtype = "float32")
+'''
+path = "example_01.jpg"
+if os.path.isfile(path):
+    img1 = cv2.imread(path, 0)
+else:
+    print ("The file " + path + " does not exist.")
+
+'''
+'''
+width 300
+height 168
+'''
+image = cv2.imread("B:\ex.jpg")
+ 
+pts = np.array([(55, 0), (170, 0), (170,60), (0, 70)], dtype = "float32")
 # apply the four point tranform to obtain a "birds eye view" of
 # the image
 warped = four_point_transform(image, pts)
